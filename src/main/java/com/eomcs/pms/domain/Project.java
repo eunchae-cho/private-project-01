a6884fe2b4959dbde491e975cc0050a3dbf8dc2b
eunchae-cho <dmsco8981@gmail.com>
2020-09-24 18:10:18 +0900

    ,

---
+++ src/main/java/com/eomcs/pms/domain/Project.java
@@ -0,0 +1,97 @@
+package com.eomcs.pms.domain;
+
+import java.sql.Date;
+
+import com.eomcs.util.CsvObject;
+
+public class Project implements CsvObject{
+	private int no;
+	private String title;
+	private String content;
+	private Date startDate;
+	private Date endDate;
+	private String owner;
+	private String members;
+
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public String getMembers() {
+		return members;
+	}
+	public void setMembers(String members) {
+		this.members = members;
+	}
+
+	@Override
+	public String toCsvString() {
+		return String.format("%d, %s, %s, %s, %s, %s, %s",
+				this.getNo(),
+				this.getTitle(),
+				this.getContent(),
+				this.getStartDate(),
+				this.getEndDate(),
+				this.getOwner(),
+				this.getMembers());
+	}
+
+	public static Project valueOfCsv(String csv) {
+		String[] fields = csv.split(",");
+		Project project = new Project();
+		project.setNo(Integer.parseInt(fields[0]));
+		project.setTitle(fields[1]);
+		project.setContent(fields[2]);
+		project.setStartDate(Date.valueOf(fields[3]));
+		project.setEndDate(Date.valueOf(fields[4]));
+		project.setOwner(fields[5]);
+		project.setMembers(fields[6]);
+		return project;
+	}
+
+	public Project() {}
+
+	public Project(String csv) {
+		String[] fields = csv.split(",");
+		this.setNo(Integer.parseInt(fields[0]));
+		this.setTitle(fields[1]);
+		this.setContent(fields[2]);
+		this.setStartDate(Date.valueOf(fields[3]));
+		this.setEndDate(Date.valueOf(fields[4]));
+		this.setOwner(fields[5]);
+		this.setMembers(fields[6]);
+	}
+
+}
---
+++ src/main/java/com/eomcs/pms/domain/Task.java
@@ -0,0 +1,77 @@
+package com.eomcs.pms.domain;
+
+import java.sql.Date;
+
+import com.eomcs.util.CsvObject;
+
+public class Task implements CsvObject{
+	private int no;
+	private String content;
+	private Date deadline;
+	private int status;
+	private String owner;
+
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	public String toCsvString() {
+		return String.format("%d,%s,%s,%d,%s", 
+				this.getNo(),
+				this.getContent(),
+				this.getDeadline(),
+				this.getStatus(),
+				this.getOwner());
+	}
+
+	public static Task valueOfCsv(String csv) {
+		String[] fields = csv.split(",");
+		Task task = new Task();
+		task.setNo(Integer.parseInt(fields[0]));
+		task.setContent(fields[1]);
+		task.setDeadline(Date.valueOf(fields[2]));
+		task.setStatus(Integer.parseInt(fields[3]));
+		task.setOwner(fields[4]);
+		return task;
+	}
+
+	public Task() {}
+
+	public Task(String csv) {
+		String[] fields = csv.split(",");
+		this.setNo(Integer.parseInt(fields[0]));
+		this.setContent(fields[1]);
+		this.setDeadline(Date.valueOf(fields[2]));
+		this.setStatus(Integer.parseInt(fields[3]));
+		this.setOwner(fields[4]);
+	}
+
+}
